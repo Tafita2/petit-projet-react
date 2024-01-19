4a75c8819bd0375ebc70d901a4013915aba682b5
Tafita <Andrinirinataf2@gmail.com>
2024-01-19 15:46:05 +0300

    Amelioration du CV

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import A_propos from "./components/A_propos";
+import Navbar from "./components/Navbar";
+import Accueil from "./components/Accueil";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Accueil />
+      <A_propos />
+      <Experience />
+      <Contact />
     </div>
   );
 }
